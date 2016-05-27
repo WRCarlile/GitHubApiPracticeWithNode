a8b94ff1343c947dce4602d10829cf7b91b569c1
Ryan Carlile <ryancarlile@gmail.com>
2016-05-27 11:40:11 -0700

    Add the beginings of a refactor

--- js/gitHubApi.js
+++ js/gitHubApi.js
@@ -1,17 +1,21 @@
 var apiKey = require('./../.env').apiKey;
 
-exports.getRepos = function(){
-  $.get('https://api.github.com/users/daneden?access_token=' + apiKey).then(function(response){
-    console.log(response);
-  }).fail(function(error){
-    console.log(error.responseJSON.message);
-  });
-};
+exports.GitHubApiCall = function() {
 
-exports.getUserName = function(){
+}
+
+// exports.GitHubApiCall.prototype.getRepos = function(){
+//   $.get('https://api.github.com/users/daneden?access_token=' + apiKey).then(function(response){
+//     console.log(response);
+//   }).fail(function(error){
+//     console.log(error.responseJSON.message);
+//   });
+// };
+
+exports.GitHubApiCall.prototype.getUserName = function(){
   $.get('https://api.github.com/users/daneden?access_token=' + apiKey).then(function(response){
-    console.log(response.login);
+    return response.login;
   }).fail(function(error){
-    console.log(error.responseJSON.message);
+    $('#showGitHubUser').text(error.responseJSON.message);
   });
 };
